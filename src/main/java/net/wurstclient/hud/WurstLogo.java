ddadbd9fb7a876f003f8f9164bab583b8f6b4ecb
Dex <sheepareawesome777@gmail.com>
2023-05-10 18:53:57 -0400

    Allow disabling WurstLogo background separately from text

--- src/main/java/net/wurstclient/hud/WurstLogo.java
+++ src/main/java/net/wurstclient/hud/WurstLogo.java
@@ -41,8 +41,8 @@ public final class WurstLogo
 		String version = getVersionString(fmt);
 		TextRenderer tr = WurstClient.MC.textRenderer;
 		
-		// draw version background if formatter not NoVersion
-		if (fmt != WurstLogoOtf.Format.NoVersion) {
+		// draw version background if enabled
+		if (otf.isBgVisible()) {
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		
